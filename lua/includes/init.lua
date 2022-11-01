ae315a85ce53091c1794866f936f7cb3b7f54749
Brandon Sturgeon <brandon@brandonsturgeon.com>
2022-11-01 14:03:11 -0700

    No newline

--- lua/includes/init.lua
+++ lua/includes/init.lua
@@ -1,5 +1,5 @@
 if ( BRANCH == "x86-64" ) then
-	print( "x86-64 branch detected, disabling LuaJIT to improve performance\n" )
+	print( "x86-64 branch detected, disabling LuaJIT to improve performance" )
 	jit.off()
 end
 
